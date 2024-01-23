9a91c951196881809fa6a4a952cb1762b6db1bd0
Matt Rangel <matt333three@aol.com>
2024-01-23 16:01:19 -0600

    Write tests for users model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user1 = User.create(email: "user1@example.com", password: "123456")
+    @user2 = User.create(email: "user2@example.com", password: "abcdef")
+  end
+
+  teardown do
+    Rails.cache.clear
+  end
+
+  test "user can follow other user" do
+    assert(@user1.follow(@user2).valid?)
+  end
+
+  test "user can't follow other user while already following" do
+    @user1.follow(@user2)
+    assert_not(@user1.follow(@user2).valid?)
+  end
+
+  test "user1 follows user2 --> relationship in user1's following list" do
+    relationship = @user1.follow(@user2)
+    assert_includes(@user1.following_relationships, relationship)
+  end
+
+  test "user1 follows user2 --> relationship in user2's followed by list" do
+    relationship = @user1.follow(@user2)
+    assert_includes(@user2.followed_by_relationships, relationship)
+  end
 end
