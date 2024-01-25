079465bc2e3053fdfc39c3c98c68ef9ff424501b
Matt Rangel <matt333three@aol.com>
2024-01-25 13:25:26 -0600

    Generate post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_20_223311) do
+ActiveRecord::Schema[7.0].define(version: 2024_01_25_192509) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "relationships", force: :cascade do |t|
     t.bigint "followed_by_id", null: false
