5a09eaf3c3d8ebcce58bae67702f85ded8b754bf
Matt Rangel <matt333three@aol.com>
2024-01-26 14:46:01 -0600

    Write method for creating new comment. Turbo stream new comment partial.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,16 @@
 class CommentsController < ApplicationController
+  def create
+    comment = current_user.comments.create(comment_params)
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.prepend("post_#{comment_params[:post_id]}_comments", partial: "comments/comment", locals: {comment: comment})
+      end
+      format.html { redirect_to root_path }
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :post_id)
+  end
 end
