e9c19cce03f49a4926a4ba51dcd7069f554fdc90
Matt Rangel <matt333three@aol.com>
2024-01-26 15:54:02 -0600

    Generate likes controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :comments, only: [:create]
+  resources :likes, only: [:create]
   root "homepage#show"
 end
