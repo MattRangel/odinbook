b869cec51ca2eb6732f504e67ae7aa90382bc36b
Matt Rangel <matt333three@aol.com>
2024-01-27 10:39:59 -0600

    Add interactive like button and count to posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,4 +1,18 @@
 class LikesController < ApplicationController
   def create
+    post = Post.find(like_params[:post_id])
+
+    Like.create(post: post, user: current_user)
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("post_#{post.id}_likes", partial: "posts/like_interaction", locals: {post: post})
+      end
+      format.html { redirect_back(fallback_location: root_path) }
+    end
+  end
+
+  def like_params
+    params.require(:like).permit(:post_id)
   end
 end
