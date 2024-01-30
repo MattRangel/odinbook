9c8f27e0460515eed864accc01f92eb3307da2e6
Matt Rangel <matt333three@aol.com>
2024-01-30 15:54:30 -0600

    Write method for creating a relationship and turbostreaming changed follow button

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,17 @@
 class RelationshipsController < ApplicationController
+  def create
+    current_user.following_relationships.create(relationship_params)
+
+    respond_to do |format|
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("user_#{relationship_params[:following_id]}_follow", partial: "users/follow", locals: {user_id: relationship_params[:following_id]})
+      end
+      format.html { redirect_back(fallback_location: root_path) }
+    end
+  end
+
+  private
+  def relationship_params
+    params.require(:relationship).permit(:following_id)
+  end
 end
