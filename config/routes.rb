d38680035707e330052727fc63760ffc7799bf61
Matt Rangel <matt333three@aol.com>
2024-01-30 16:01:53 -0600

    Write method and route needed for showing user page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:show]
   resources :comments, only: [:create]
+  resources :relationships, only: [:create]
   resources :likes, only: [:create]
   resources :posts, only: [:create, :new, :show]
   root "homepage#show"
