df442ad184c1ea81792b852bcdc39178ab220342
Matt Rangel <matt333three@aol.com>
2024-01-31 19:30:18 -0600

    Write methods and routes for editing and updating user display name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,18 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @posts = Post.feed(@user.id)
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    current_user.update(name_param)
+    redirect_to user_path
+  end
+
+  private
+  def name_param
+    params.require(:user).permit(:name)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
   resources :comments, only: [:create]
   resources :relationships, only: [:create]
   resources :likes, only: [:create]
