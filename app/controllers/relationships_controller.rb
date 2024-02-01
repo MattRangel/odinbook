02e9844aeb0a9d9ae040a71bbf51d4bbaa78583f
Matt Rangel <matt333three@aol.com>
2024-01-31 19:07:10 -0600

    Render follow button turbo stream more concisely

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,13 +1,7 @@
 class RelationshipsController < ApplicationController
   def create
     current_user.following_relationships.create(relationship_params)
-
-    respond_to do |format|
-      format.turbo_stream do
-        render turbo_stream: turbo_stream.replace("user_#{relationship_params[:following_id]}_follow", partial: "users/follow", locals: {user_id: relationship_params[:following_id]})
-      end
-      format.html { redirect_back(fallback_location: root_path) }
-    end
+    render partial: "users/follow", locals: {user_id: relationship_params[:following_id]}
   end
 
   private
