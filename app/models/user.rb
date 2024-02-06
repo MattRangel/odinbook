d9c6a01d31221a9cd016def85e23afcc577fb0d2
Matt Rangel <matt333three@aol.com>
2024-02-06 15:26:20 -0600

    Make users model omniauthable with github

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  devise :omniauthable, omniauth_providers: %i[github]
 
   validates :bio, length: { maximum: 100 }
   validates :name, length: { maximum: 12 }, presence: true
