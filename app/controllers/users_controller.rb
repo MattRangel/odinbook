e5a53d787143d10e44ada87a4c8c19438b742786
Matt Rangel <matt333three@aol.com>
2024-02-09 15:26:23 -0600

    Add index view to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,10 @@ class UsersController < ApplicationController
     redirect_to user_path
   end
 
+  def index
+    @users = User.all
+  end
+
   private
   def user_params
     params.require(:user).permit(:name, :bio)
