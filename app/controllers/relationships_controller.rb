ac62512edffffa0ecb6feddf69b10e40086c0f79
Matt Rangel <matt333three@aol.com>
2024-02-13 15:24:43 -0600

    Update relationships controller with view methods, and methods for accepting and rejecting requests

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,8 +4,32 @@ class RelationshipsController < ApplicationController
     render partial: "users/follow", locals: {user_id: relationship_params[:following_id]}
   end
 
+  def destroy
+    relationship = current_user.followed_by_relationships.find_by(followed_by_id: relationship_params[:followed_by_id])
+    relationship.destroy unless relationship.nil?
+    render turbo_stream: turbo_stream.remove("user_#{relationship_params[:followed_by_id]}_response")
+  end
+
+  def update
+    relationship = current_user.followed_by_relationships.where(relationship_params)
+    relationship.update(accepted: true) unless relationship.nil?
+    render turbo_stream: turbo_stream.remove("user_#{relationship_params[:followed_by_id]}_response")
+  end
+
+  def following
+    @users = current_user.following_users
+  end
+
+  def followed_by
+    @users = current_user.followed_by_users_accepted
+  end
+
+  def requests
+    @users = current_user.followed_by_users_pending
+  end
+
   private
   def relationship_params
-    params.require(:relationship).permit(:following_id)
+    params.require(:relationship).permit(:following_id, :followed_by_id)
   end
 end
