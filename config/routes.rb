962fbb877232d50057a553d1093403d6af8232ef
Matt Rangel <matt333three@aol.com>
2024-02-13 15:40:02 -0600

    Add routes for relationship views, and accepting / rejecting follow requests

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resources :users, only: [:show, :edit, :update, :index]
   resources :comments, only: [:create]
-  resources :relationships, only: [:create]
+  resource :relationships, only: [:create, :update, :destroy] do
+    collection do
+      get 'following'
+      get 'followed_by'
+      get 'requests'
+    end
+  end
   resources :likes, only: [:create]
   resources :posts, only: [:create, :new, :show]
   root "homepage#show"
