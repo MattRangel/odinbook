c989af398e39f29ce28d4eebdee7488fabea4997
Matt Rangel <matt333three@aol.com>
2024-04-08 15:20:04 -0500

    Add image_url to allowed parameters for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,6 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:body)
+    params.require(:post).permit(:body, :image_url)
   end
 end
